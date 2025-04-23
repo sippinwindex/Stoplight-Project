08ee9f8f27ffb9e9b4990b1f700b216da298f9dc
Juan Fernandez <stormblazart@gmail.com>
2025-04-23 18:02:42 -0400

    Update and rename Home.jsx to App.jsx

---
+++ src/js/components/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TrafficLight from "./TrafficLight";
+
+const App = () => {
+  return (
+    <div>
+      <TrafficLight />
+    </div>
+  );
+};
+
+export default App;
